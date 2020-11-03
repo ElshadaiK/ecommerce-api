e73f50e43f8a4ec606f6d8c49eee9e73f56000ce
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2020-11-03 13:02:54 +0300

    Removing items finished

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -81,73 +81,60 @@ exports.addToCart = async (req, res, next) => {
 
 exports.removeFromCart = async (req, res, next) => {
     const {user} = req
-    const {itemId, quantity} = req.body
+    const {itemId, quantity_to} = req.body
     try{
 
         if(user){
+            const user_id = user.data._id
             const item = await productModel.findById(itemId);
-            if(item){
-                const user_id = user.data._id;
-                if(item.quantity == 0) throw new Error("Store out of the item you requested")
-                if(item.quantity >= quantity){
-                    const newlyAddedItems = {
-                        item: itemId,
-                        amount: quantity
-                    }
-                    const remaining_amount = item.quantity - quantity;
-                    let items_found = false
-                    let available_items;
-                    let cart = await cartModel.findOne({user: user_id, status: 'pending'})
-                    if(!cart){
-                        // If there is no cart with pending state
-                        cart = await cartModel.create({user: user_id, status: 'pending'})
-                    }else{
-                        // If there is the item added in the cart before
-                        available_items = cart.items
-                        const found = available_items.findIndex(available_item => available_item.item == itemId)
-                        if(found !== -1){
-                            items_found = true
-                            available_items[found].amount = +available_items[found].amount + +quantity 
-                            }
-                    }
-                    // Updating the cart
-                    let updatedCart;
-                    if(!items_found){
-                        updatedCart = await cartModel.findOneAndUpdate(
-                            {user: user_id, status: 'pending'},
-                            {$push: {items: newlyAddedItems}},
-                            {new: true}
-                        );
-                    }
-                    else{
-                        // If the item is already there 
-                        console.log(available_items)
-                        updatedCart = await cartModel.findOneAndUpdate(
-                            {user: user_id, status: 'pending'},
-                            {items: available_items},
-                            {new: true}
-                        )
-                    }
-                    // Update the store
-                    const updated_item = await productModel.updateOne(
-                        {_id: itemId},
-                        {quantity: remaining_amount}
-                        );
-                    res.json(updatedCart);
-                    next();
+            const cart = await cartModel.findOne({user: user_id, status: 'pending'})
+
+            if(!cart) throw new Error("You don't have a cart");
+
+
+            available_items = cart.items
+
+            const found = available_items.findIndex(available_item => available_item.item == itemId)
+            if(found !== -1){
+
+                if(quantity_to >= available_items[found].amount) throw new Error("You don't have this amount of item in your cart")
+
+                // update cart
+                const store_amount = item.quantity + (+available_items[found].amount - quantity_to)
+                
+                let updatedCart;
+                if(quantity_to != 0){
+                    available_items[found].amount = +quantity_to
+                    updatedCart = await cartModel.findOneAndUpdate(
+                        {user: user_id, status: 'pending'},
+                        {items: available_items},
+                        {new: true}
+                    )                
                 }
                 else{
-                    throw new Error(`There aren't enough quantities available. The maximun you can buy is ${item.quantity}`)
+                    updatedCart = await cartModel.findOneAndUpdate(
+                        {user: user_id, status: 'pending'},
+                        {$pull: {items: available_items[found]}},
+                        {new: true}
+                    );
                 }
+
+                // update store
+                const updated_item = await productModel.updateOne(
+                    {_id: itemId},
+                    {quantity: store_amount}
+                    );
+                
+                res.json(updatedCart)
+                next;
             }
             else{
-                throw new Error('Item not found') 
+                throw new Error("Item not found in your cart")
             }
         }
         else{
             throw new Error('You have to login first') 
         }
-
     }
     
     catch (err) {
